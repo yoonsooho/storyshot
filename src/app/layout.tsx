0c4451c03e79cf9d4dea1d9f99be6de80e25085a
yoonsooho <7552678@naver.com>
2026-02-11 02:15:22 +0900

    feat : 구글 에드센스 script 코드추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -72,7 +73,16 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ko">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <Script
+                    id="adsbygoogle-init"
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8116400352006173"
+                    crossOrigin="anonymous"
+                    strategy="afterInteractive"
+                />
+                {children}
+            </body>
         </html>
     );
 }
